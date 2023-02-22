086c60bee62cd129f07500709660d1f36ce6897f
sami2022skipq <abdul.sami.skipq@gmail.com>
2023-02-22 15:04:47 +0500

    final commit

--- react-components/src/MoviesList.js
+++ react-components/src/MoviesList.js
@@ -2,7 +2,7 @@ const MoviesList =({item})=>{
     return (
         <li className="movie" key = {item.id}>
             <img src={item.poster} alt ={item.title} />
-            <h4>{item.title}  director by {item.director} was released on {item.year}</h4>
+            <h4>{item.title} by {item.director} was released on {item.year}</h4>
             <h4>Rated :{item.rating}</h4> 
         </li>
     )
