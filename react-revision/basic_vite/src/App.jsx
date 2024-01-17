3815153eff5b19054b1da9fa285144df3839bb27
sami2022skipq <abdul.sami.skipq@gmail.com>
2024-01-17 22:08:56 +0500

    commit

---
+++ react-revision/basic_vite/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from "./Youtube"
+
+function App() {
+
+  return (
+    <>
+      <Youtube/>
+      <h1>Vite is here</h1>          
+    </>
+  )
+}
+
+export default App
---
+++ react-revision/basic_vite/src/Youtube.jsx
@@ -0,0 +1,9 @@
+function Youtube(){
+    return (
+        <h1>
+            This is a Youtube Componenet
+        </h1>
+    )
+}
+export default Youtube
+
