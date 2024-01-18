a839e1f4654f30c03e1eba0ad19263e1ed3b4c56
sami2022skipq <abdul.sami.skipq@gmail.com>
2024-01-18 20:28:27 +0500

    card updated

--- react-revision/props_and _tailwind/src/App.jsx
+++ react-revision/props_and _tailwind/src/App.jsx
@@ -8,11 +8,15 @@ function App() {
 
   return (
     <>
-    <h1 className='text-3xl font-bold bg-blue-300 rounded-lg underline'>This is TailwindCss : {count}</h1>
+    <h1 className='bg-blue-300 rounded-md underline flex-auto'>Creating a card</h1>
     <Card name ={"Abdul Sami"} designation= {"Senior Software Engineer"}city= {"Islamabad"}/>
+    <Card name ={"Muhammad Usman"} designation= {"HR Manager"}city= {"Islamabad"}/>
+    <Card name ={"Azaad Imran"} designation= {"Project Manager"}city= {"Islamabad"}/>
     
     </>
   )
 }
 
 export default App
+
+
