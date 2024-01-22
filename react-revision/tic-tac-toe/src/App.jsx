9edabe236883f64b3bdbbe91886454d103a9c616
sami2022skipq <abdul.sami.skipq@gmail.com>
2024-01-22 21:48:49 +0500

    Winner added

--- react-revision/tic-tac-toe/src/App.jsx
+++ react-revision/tic-tac-toe/src/App.jsx
@@ -12,8 +12,19 @@ function Square({ value, onSquareClick }) {
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, SetXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  winner
+    ? (status = "Winner: " + winner)
+    : (status = "Next Player: " + (xIsNext ? "X" : "0"));
+
   function handleClick(i) {
-    if (squares[i]) return;
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+
     const nextSquares = squares.slice();
 
     xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
@@ -24,6 +35,7 @@ function Board() {
 
   return (
     <>
+      <div>{status}</div>
       <div className="board">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -44,3 +56,24 @@ function Board() {
 }
 
 export default Board;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
