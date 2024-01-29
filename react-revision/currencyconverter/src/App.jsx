632c31a6bd5ff3875d109ba16ca506f2121c5942
sami2022skipq <abdul.sami.skipq@gmail.com>
2024-01-29 21:36:25 +0500

    initial commit

---
+++ react-revision/currencyconverter/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      className="w-full  h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`,
+      }}
+    >
+      <h1 className="bg-gray-300">Currency Converter</h1>
+    </div>
+  );
+}
+
+export default App;
