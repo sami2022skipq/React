9714512b6b6f9933bbb16601933c1813a82c4fa5
sami2022skipq <abdul.sami.skipq@gmail.com>
2024-02-05 19:07:58 +0500

    initial commit

---
+++ react-revision/react-router-dom/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-slate-500 text-white text-center  text-3xl rounded-3xl m-4 p-4 underline-offset-1'>React Router Dom</h1>
+    </>
+  )
+}
+
+export default App
