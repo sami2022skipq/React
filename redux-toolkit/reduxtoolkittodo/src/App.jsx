b958bf35b0e13155a821dc67a930d8c7b1075a28
sami2022skipq <abdul.sami.skipq@gmail.com>
2024-03-25 14:50:42 +0500

    drop down list component added

--- redux-toolkit/reduxtoolkittodo/src/App.jsx
+++ redux-toolkit/reduxtoolkittodo/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import AddToDo from "./components/AddToDo";
+import TodoDropdown from "./components/TodoDropodown";
 import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl bg-slate-400">Learn React Redux..</h1>;
+      <h1 className="text-3xl bg-slate-400">Learn React Redux..</h1>
       <AddToDo />
+      <TodoDropdown/>
       <Todos />
     </>
   );
